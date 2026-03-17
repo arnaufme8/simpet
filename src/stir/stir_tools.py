64dd9f3d26c1fc4e1e2cf42762ffb58b5feb6a88
arnaufme8 <113554412+arnaufme8@users.noreply.github.com>
2026-03-17 23:12:20 +0100

    Adding file for Total-Body: stir src

--- src/stir/stir_tools.py
+++ src/stir/stir_tools.py
@@ -8,6 +8,8 @@ import random
 from utils import tools
 from utils import resources as rsc
 
+import stir
+
 
 def create_stir_hs_from_detparams(scannerParams, output_file, output_format="SimSET"):
     num_rings = scannerParams.get("num_rings")
@@ -153,6 +155,154 @@ def create_stir_hs_from_detparams(scannerParams, output_file, output_format="Sim
         + "start horizontal bed position (mm) := 0\n"
         + "!END OF INTERFILE :=\n"
     )
+
+
+def create_reduced_stir_hs_from_detparams(scannerParams, output_file, output_format="SimSET"):
+    num_rings = scannerParams.get("num_rings")
+    max_z = scannerParams.get("axial_fov") / 2
+    min_z = -scannerParams.get("axial_fov") / 2
+    z_crystal_size = scannerParams.get("z_crystal_size")
+    gap_size = (max_z - min_z - z_crystal_size * num_rings) / (num_rings - 1)
+    ring_spacing = z_crystal_size + gap_size
+    max_segment = scannerParams.get("max_segment")
+
+    min_td = -scannerParams.get("scanner_radius")
+    max_td = scannerParams.get("scanner_radius")
+    td_bins = scannerParams.get("num_td_bins")
+    bin_size = (max_td - min_td) / float(td_bins)
+    matrix_size, ring_difference = generate_segments_lists_stir(
+        num_rings, max_segment #- 1
+    )
+    transaxial_crystal_distance = scannerParams.get("transaxial_crystal_size")
+
+    scanner_name = scannerParams.get("scanner_name")
+
+    if output_format == "SimSET":
+        views_coordinate = 2
+        axial_coordinate = 3
+    else:
+        views_coordinate = 3
+        axial_coordinate = 2
+
+    new_file = open(output_file, "w")
+
+    new_file.write(
+        "!INTERFILE  :=\n"
+        + "!imaging modality := PT\n"
+        + "name of data file := "
+        + output_file[0:-2]
+        + "s"
+        + "\n"
+        + "originating system := "
+        + scanner_name
+        + "\n"
+        + "!version of keys := STIR3.0\n"
+        + "!GENERAL DATA :=\n"
+        + "!GENERAL IMAGE DATA :=\n"
+        + "!type of data := PET\n"
+        + "imagedata byte order := LITTLEENDIAN\n"
+        + "!PET STUDY (General) :=\n"
+        + "!PET data type := Emission\n"
+        + "applied corrections := {arc correction}\n"
+        + "!number format := float\n"
+        + "!number of bytes per pixel := 4\n"
+        + "number of dimensions := 4\n"
+        + "matrix axis label [4] := segment\n"
+        + "!matrix size [4] := "
+        + str(2 * max_segment + 1) #THIS CHANGED TO MAX_SEGMENT
+        + "\n"
+        + "matrix axis label ["
+        + str(views_coordinate)
+        + "] := view\n"
+        + "!matrix size ["
+        + str(views_coordinate)
+        + "] := "
+        + str(scannerParams.get("num_aa_bins"))
+        + "\n"
+        + "matrix axis label ["
+        + str(axial_coordinate)
+        + "] := axial coordinate\n"
+        + "!matrix size ["
+        + str(axial_coordinate)
+        + "] := "
+        + matrix_size #THIS NEEDS TO CHANGE
+        + "\n"
+        + "matrix axis label [1] := tangential coordinate\n"
+        + "!matrix size [1] := "
+        + str(td_bins)
+        + "\n"
+        "minimum ring difference per segment := "
+        + ring_difference #THIS NEEDS TO CHANGE
+        + "\n"
+        + "maximum ring difference per segment := "
+        + ring_difference #THIS NEEDS TO CHANGE
+        + "\n"
+        + "Scanner parameters:= \n"
+        + "Scanner type := "
+        + scanner_name
+        + "\n"
+        + "Number of rings := "
+        + str(num_rings) #THIS NEEDS TO CHANGE??? (or maybe not)
+        + "\n"
+        + "Number of detectors per ring := "
+        + str(scannerParams.get("num_aa_bins") * 2) #THIS NEEDS TO CHANGE??? (or maybe not)
+        + "\n"
+        "Inner ring diameter (cm) := "
+        + str(scannerParams.get("scanner_radius") * 2)
+        + "\n"
+        + "Average depth of interaction (cm) := "
+        + str(scannerParams.get("average_doi"))
+        + "\n"
+        + "Distance between rings (cm) := "
+        + str(ring_spacing)
+        + "\n"
+        + "Default bin size (cm) := "
+        + str(bin_size)
+        + "\n"
+        + "View offset (degrees) := 0\n"
+        + "Maximum number of non-arc-corrected bins := "
+        + str(td_bins)
+        + "\n"
+        + "Default number of arc-corrected bins := "
+        + str(td_bins)
+        + "\n"
+        + "Energy_resolution := "
+        + str(scannerParams.get("energy_resolution") / 100)
+        + "\n"
+        + "Reference energy (in keV) := 511\n"
+        + "Number of blocks per bucket in transaxial direction := 1\n"
+        + "Number of blocks per bucket in axial direction := 1\n"
+        + "Number of crystals per block in axial direction := 1\n"
+        + "Number of crystals per block in transaxial direction := 1\n"
+        + "Number of crystals per singles unit in axial direction := 1\n"
+        + "Number of crystals per singles unit in transaxial direction := 1\n"
+        + "Scanner geometry := Cylindrical"
+        + "\n"
+        + "Distance between crystals in axial direction (cm) := "
+        + str(ring_spacing)
+        + "\n"
+        + "Distance between crystals in transaxial direction (cm) := "
+        + str(transaxial_crystal_distance)
+        + "\n"
+        + "Distance between blocks in transaxial direction (cm) := "
+        + str(transaxial_crystal_distance)
+        + "\n"
+        + "Distance between crystals in axial direction (cm) := "
+        + str(ring_spacing)
+        + "\n"
+        + "Distance between blocks in axial direction (cm) := "
+        + str(ring_spacing)
+        + "\n"
+        + "end scanner parameters:=\n"
+        + "effective central bin size (cm) := "
+        + str(bin_size)
+        + "\n"
+        + "number of time frames := 1\n"
+        + "start vertical bed position (mm) := 0\n"
+        + "start horizontal bed position (mm) := 0\n"
+        + "!END OF INTERFILE :=\n"
+    )
+
 
 
 def generate_segments_lists_stir(nrings, max_segment):
@@ -558,7 +708,8 @@ def OSEM3D_recons(
     numberOfSubsets = scannerParams.get("numberOfSubsets")
     numberOfIterations = scannerParams.get("numberOfIterations")
     savingInterval = scannerParams.get("savingInterval")
-
+    
+    """ #RESTABLISH IF FAILS
     if scannerParams.get("stir_recons_att_corr") == 1:
         att_corr_str = (
             "Bin Normalisation type := From ProjData \n"
@@ -570,7 +721,29 @@ def OSEM3D_recons(
         )
     else:
         att_corr_str = ""
-
+    """
+    
+    if scannerParams.get("stir_norm_from_att_map") == 1:
+        att_corr_str = (
+            "Bin Normalisation From Attenuation Image:= \n"
+            + "attenuation_image_filename := " 
+            + join(output_dir, 'mu_map.hv')
+            + "\n"
+            + "End Bin Normalisation From Attenuation Image := \n"
+        )
+    elif scannerParams.get("stir_recons_att_corr") == 1:
+        att_corr_str = (
+            "Bin Normalisation type := From ProjData \n"
+            + "Bin Normalisation From ProjData := \n"
+            + "normalisation projdata filename:= "
+            + att_stir
+            + "\n"
+            + "End Bin Normalisation From ProjData:= \n"
+        )
+    else:
+        att_corr_str = ""
+    
+    
     if (
         scannerParams.get("stir_scatt_corr_smoothing") == 1
     ):  # Will use smoothed SimSET scatter as additive_sinogram.
@@ -672,12 +845,23 @@ def OSEM3D_recons(
     )
 
     new_file.close()
-
+    
+    #RESTABLISH IF FAILS!!!!
     command = "%s %s >> %s" % (recons, paramsFile, log_file)
     tools.osrun(command, log_file)
+    
+    #recon=stir.OSMAPOSLReconstruction3DFloat(paramsFile)
+    
+    #print("ATTRIBUTES:", dir(recon))
+    
+    #target=recon.get_initial_data()
+    #target.fill(1)
+    #s=recon.set_up(target)
+    #recon.reconstruct(target)
 
     output = recFileName + "_" + str(scannerParams.get("numberOfIterations")) + ".hv"
 
-    output = tools.anything_to_hdr_convert(output, log_file)
+    #output = tools.anything_to_hdr_convert(output, log_file)
+    output = tools.convert_hv_to_nii(output, log_file)
 
     return output
