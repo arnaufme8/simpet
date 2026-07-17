6b07b38d6790f73ee97d5f187e96b9a8f0f0d761
arnaufme8 <113554412+arnaufme8@users.noreply.github.com>
2026-07-17 22:07:39 +0200

    Add files via upload

--- src/stir/stir_tools.py
+++ src/stir/stir_tools.py
@@ -8,9 +8,7 @@ import random
 from utils import tools
 from utils import resources as rsc
 
-import stir
-
-
+#OLD VERSION. MUST BE UPDATED FOR STIR 6.0...
 def create_stir_hs_from_detparams(scannerParams, output_file, output_format="SimSET"):
     num_rings = scannerParams.get("num_rings")
     max_z = scannerParams.get("axial_fov") / 2
@@ -18,14 +16,19 @@ def create_stir_hs_from_detparams(scannerParams, output_file, output_format="Sim
     z_crystal_size = scannerParams.get("z_crystal_size")
     gap_size = (max_z - min_z - z_crystal_size * num_rings) / (num_rings - 1)
     ring_spacing = z_crystal_size + gap_size
-
+    
+    max_segment = scannerParams.get("max_segment")
+    
     min_td = -scannerParams.get("scanner_radius")
     max_td = scannerParams.get("scanner_radius")
     td_bins = scannerParams.get("num_td_bins")
     bin_size = (max_td - min_td) / float(td_bins)
-    matrix_size, ring_difference = generate_segments_lists_stir(
+    matrix_size, ring_difference = generate_segments_lists_stir( #WARNING! RECOVER THIS IF STOPS WORKING.
         num_rings, num_rings - 1
     )
+    #matrix_size, ring_difference = generate_segments_lists_stir( #WARNING! THIS IS NEEDED FOR FBP3D
+    #    num_rings, max_segment
+    #)
     transaxial_crystal_distance = scannerParams.get("transaxial_crystal_size")
 
     scanner_name = scannerParams.get("scanner_name")
@@ -62,7 +65,7 @@ def create_stir_hs_from_detparams(scannerParams, output_file, output_format="Sim
         + "number of dimensions := 4\n"
         + "matrix axis label [4] := segment\n"
         + "!matrix size [4] := "
-        + str(2 * (num_rings - 1) + 1)
+        + str(2 * (num_rings - 1) + 1) #WARNING! RECOVER THIS: str(2 * (num_rings - 1) + 1) #WARNING! THIS IS NEEDED FOR FBP3D: str(2 * (max_segment) + 1)
         + "\n"
         + "matrix axis label ["
         + str(views_coordinate)
@@ -157,169 +160,47 @@ def create_stir_hs_from_detparams(scannerParams, output_file, output_format="Sim
     )
 
 
-def create_reduced_stir_hs_from_detparams(scannerParams, output_file, output_format="SimSET"):
-    num_rings = scannerParams.get("num_rings")
-    max_z = scannerParams.get("axial_fov") / 2
-    min_z = -scannerParams.get("axial_fov") / 2
-    z_crystal_size = scannerParams.get("z_crystal_size")
-    gap_size = (max_z - min_z - z_crystal_size * num_rings) / (num_rings - 1)
-    ring_spacing = z_crystal_size + gap_size
-    max_segment = scannerParams.get("max_segment")
-
-    min_td = -scannerParams.get("scanner_radius")
-    max_td = scannerParams.get("scanner_radius")
-    td_bins = scannerParams.get("num_td_bins")
-    bin_size = (max_td - min_td) / float(td_bins)
-    matrix_size, ring_difference = generate_segments_lists_stir(
-        num_rings, max_segment #- 1
-    )
-    transaxial_crystal_distance = scannerParams.get("transaxial_crystal_size")
-
-    scanner_name = scannerParams.get("scanner_name")
-
-    if output_format == "SimSET":
-        views_coordinate = 2
-        axial_coordinate = 3
-    else:
-        views_coordinate = 3
-        axial_coordinate = 2
-
-    new_file = open(output_file, "w")
-
-    new_file.write(
-        "!INTERFILE  :=\n"
-        + "!imaging modality := PT\n"
-        + "name of data file := "
-        + output_file[0:-2]
-        + "s"
-        + "\n"
-        + "originating system := "
-        + scanner_name
-        + "\n"
-        + "!version of keys := STIR3.0\n"
-        + "!GENERAL DATA :=\n"
-        + "!GENERAL IMAGE DATA :=\n"
-        + "!type of data := PET\n"
-        + "imagedata byte order := LITTLEENDIAN\n"
-        + "!PET STUDY (General) :=\n"
-        + "!PET data type := Emission\n"
-        + "applied corrections := {arc correction}\n"
-        + "!number format := float\n"
-        + "!number of bytes per pixel := 4\n"
-        + "number of dimensions := 4\n"
-        + "matrix axis label [4] := segment\n"
-        + "!matrix size [4] := "
-        + str(2 * max_segment + 1) #THIS CHANGED TO MAX_SEGMENT
-        + "\n"
-        + "matrix axis label ["
-        + str(views_coordinate)
-        + "] := view\n"
-        + "!matrix size ["
-        + str(views_coordinate)
-        + "] := "
-        + str(scannerParams.get("num_aa_bins"))
-        + "\n"
-        + "matrix axis label ["
-        + str(axial_coordinate)
-        + "] := axial coordinate\n"
-        + "!matrix size ["
-        + str(axial_coordinate)
-        + "] := "
-        + matrix_size #THIS NEEDS TO CHANGE
-        + "\n"
-        + "matrix axis label [1] := tangential coordinate\n"
-        + "!matrix size [1] := "
-        + str(td_bins)
-        + "\n"
-        "minimum ring difference per segment := "
-        + ring_difference #THIS NEEDS TO CHANGE
-        + "\n"
-        + "maximum ring difference per segment := "
-        + ring_difference #THIS NEEDS TO CHANGE
-        + "\n"
-        + "Scanner parameters:= \n"
-        + "Scanner type := "
-        + scanner_name
-        + "\n"
-        + "Number of rings := "
-        + str(num_rings) #THIS NEEDS TO CHANGE??? (or maybe not)
-        + "\n"
-        + "Number of detectors per ring := "
-        + str(scannerParams.get("num_aa_bins") * 2) #THIS NEEDS TO CHANGE??? (or maybe not)
-        + "\n"
-        "Inner ring diameter (cm) := "
-        + str(scannerParams.get("scanner_radius") * 2)
-        + "\n"
-        + "Average depth of interaction (cm) := "
-        + str(scannerParams.get("average_doi"))
-        + "\n"
-        + "Distance between rings (cm) := "
-        + str(ring_spacing)
-        + "\n"
-        + "Default bin size (cm) := "
-        + str(bin_size)
-        + "\n"
-        + "View offset (degrees) := 0\n"
-        + "Maximum number of non-arc-corrected bins := "
-        + str(td_bins)
-        + "\n"
-        + "Default number of arc-corrected bins := "
-        + str(td_bins)
-        + "\n"
-        + "Energy_resolution := "
-        + str(scannerParams.get("energy_resolution") / 100)
-        + "\n"
-        + "Reference energy (in keV) := 511\n"
-        + "Number of blocks per bucket in transaxial direction := 1\n"
-        + "Number of blocks per bucket in axial direction := 1\n"
-        + "Number of crystals per block in axial direction := 1\n"
-        + "Number of crystals per block in transaxial direction := 1\n"
-        + "Number of crystals per singles unit in axial direction := 1\n"
-        + "Number of crystals per singles unit in transaxial direction := 1\n"
-        + "Scanner geometry := Cylindrical"
-        + "\n"
-        + "Distance between crystals in axial direction (cm) := "
-        + str(ring_spacing)
-        + "\n"
-        + "Distance between crystals in transaxial direction (cm) := "
-        + str(transaxial_crystal_distance)
-        + "\n"
-        + "Distance between blocks in transaxial direction (cm) := "
-        + str(transaxial_crystal_distance)
-        + "\n"
-        + "Distance between crystals in axial direction (cm) := "
-        + str(ring_spacing)
-        + "\n"
-        + "Distance between blocks in axial direction (cm) := "
-        + str(ring_spacing)
-        + "\n"
-        + "end scanner parameters:=\n"
-        + "effective central bin size (cm) := "
-        + str(bin_size)
-        + "\n"
-        + "number of time frames := 1\n"
-        + "start vertical bed position (mm) := 0\n"
-        + "start horizontal bed position (mm) := 0\n"
-        + "!END OF INTERFILE :=\n"
-    )
-
-
-
 def generate_segments_lists_stir(nrings, max_segment):
     last_segment_sinograms = nrings - max_segment
     my_matrix_size = " "
     my_matrix_ring_difference = " "
     for i in range(last_segment_sinograms, nrings):
         my_matrix_size = my_matrix_size + str(i) + ","
-        my_matrix_ring_difference = my_matrix_ring_difference + str(i - nrings) + ","
+        my_matrix_ring_difference = my_matrix_ring_difference + str(i - nrings) + "," #WARNING! RECOVER IF FAILS.
+        #my_matrix_ring_difference = my_matrix_ring_difference + str(nrings - i) + "," #NOTE: suspect rings are inverted.
     for i in range(nrings, last_segment_sinograms - 1, -1):
         my_matrix_size = my_matrix_size + str(i) + ","
-        my_matrix_ring_difference = my_matrix_ring_difference + str(nrings - i) + ","
+        my_matrix_ring_difference = my_matrix_ring_difference + str(nrings - i) + "," #WARNING! RECOVER IF FAILS.
+        #my_matrix_ring_difference = my_matrix_ring_difference + str(i - nrings) + "," #NOTE: suspect rings are inverted.
 
     my_matrix_size = "{" + my_matrix_size[0:-1] + "}"
     my_matrix_ring_difference = "{" + my_matrix_ring_difference[0:-1] + "}"
 
     return my_matrix_size, my_matrix_ring_difference
+
+
+def generate_segments_lists_stir_v2(nrings, max_segment):
+    my_matrix_size = " "
+    for i in range(1, nrings):
+        my_matrix_size = my_matrix_size + str(i) + ","
+    for i in range(nrings, 0, -1):
+        my_matrix_size = my_matrix_size + str(i) + ","
+    
+    last_segment_sinograms = nrings - max_segment
+    my_matrix_ring_difference = " "
+    for i in range(last_segment_sinograms, nrings):
+        my_matrix_ring_difference = my_matrix_ring_difference + str(i - nrings) + ","
+    for i in range(nrings, last_segment_sinograms - 1, -1):
+        my_matrix_ring_difference = my_matrix_ring_difference + str(nrings - i) + ","
+
+    my_matrix_size = "{" + my_matrix_size[0:-1] + "}"
+    my_matrix_ring_difference = "{" + my_matrix_ring_difference[0:-1] + "}"
+    
+    print("MY MATRIX SIZE:", my_matrix_size)
+    print("MY MATRIX RING DIFFERENCE:", my_matrix_ring_difference)
+
+    return my_matrix_size, my_matrix_ring_difference
+
 
 
 def apply_psf(scannerParams, sinogram_stir, log_file):
@@ -441,7 +322,10 @@ def FBP2D_recons(config, scannerParams, sinograms_stir, output_dir, log_file):
     tools.osrun(command, log_file)
 
     output = recFileName + ".hv"
-    output = tools.anything_to_hdr_convert(output, log_file)
+    #output = tools.anything_to_hdr_convert(output, log_file)
+    
+    output = tools.convert_hv_to_nii(output, log_file)
+    tools.flip_rec_nifti(recFileName + ".nii", join(output_dir, "reconstruction.nii")) #WARNING! REMOVE IF THIS FAILS. THIS FLIPS THE FINAL REC IN 0 AXIS
 
     return output
 
@@ -452,6 +336,7 @@ def FBP3D_recons(config, scannerParams, sinograms_stir, output_dir, log_file):
 
     zoom = scannerParams.get("zoomFactor")
     xyOutputSize = scannerParams.get("xyOutputSize")
+    max_segment = scannerParams.get("max_segment")
 
     recFileName = join(output_dir, "rec_FBP3D")
 
@@ -471,20 +356,23 @@ def FBP3D_recons(config, scannerParams, sinograms_stir, output_dir, log_file):
         + "xy output image size (in pixels) := "
         + str(xyOutputSize)
         + "\n\n"
-        "maximum absolute segment number to process := 2 \n"
+        #"maximum absolute segment number to process := 2 \n"
+        #+ "maximum absolute segment number to process := -1 \n"
+        + "maximum absolute segment number to process := " + str(max_segment) + "\n"
         + "num segments to combine with ssrb := -1 \n\n"
+        #+ "num segments to combine with ssrb := 3 \n\n"
         + "alpha parameter for ramp filter := 1 \n"
         + "cut-off for ramp filter (in cycles) := 0.5 \n\n"
-        + "alpha parameter for colsher filter in axial direction := 1 \n"
-        + "cut-off for colsher filter in axial direction (in cycles) := 0.5 \n"
-        + "alpha parameter for colsher filter in planar direction := 1 \n"
-        + "cut-off for colsher filter in planar direction (in cycles) := 0.5 \n\n"
-        + "stretch factor for colsher filter definition in axial direction := 2 \n"
-        + "stretch factor for colsher filter definition in planar direction := 2 \n\n"
-        + "transaxial extension for fft := 1 \n"
-        + "axial extension for fft := 1 \n\n"
-        + "save intermediate images := 0 \n"
-        + "display level := 0 \n\n"
+        #+ "alpha parameter for colsher filter in axial direction := 1 \n"
+        #+ "cut-off for colsher filter in axial direction (in cycles) := 0.5 \n"
+        #+ "alpha parameter for colsher filter in planar direction := 1 \n"
+        #+ "cut-off for colsher filter in planar direction (in cycles) := 0.5 \n\n"
+        #+ "stretch factor for colsher filter definition in axial direction := 2 \n"
+        #+ "stretch factor for colsher filter definition in planar direction := 2 \n\n"
+        #+ "transaxial extension for fft := 1 \n"
+        #+ "axial extension for fft := 1 \n\n"
+        #+ "save intermediate images := 0 \n"
+        #+ "display level := 0 \n\n"
         + "end := \n"
     )
 
@@ -494,7 +382,10 @@ def FBP3D_recons(config, scannerParams, sinograms_stir, output_dir, log_file):
     tools.osrun(command, log_file)
 
     output = recFileName + ".hv"
-    output = tools.anything_to_hdr_convert((output))
+    #output = tools.anything_to_hdr_convert((output))
+    
+    output = tools.convert_hv_to_nii(output, log_file)
+    tools.flip_rec_nifti(recFileName + ".nii", join(output_dir, "reconstruction.nii")) #WARNING! REMOVE IF THIS FAILS. THIS FLIPS THE FINAL REC IN 0 AXIS
 
     return output
 
@@ -566,7 +457,7 @@ def OSEM2D_recons(
     zoom_aux = 1
     xyOutputSize_aux = round(xyOutputSize / zoom)
 
-    if scannerParams.get("analytical_att_correction") == 1:
+    if scannerParams.get("analytical_att_correction") == 1: #CHECK THIS!!!!
         att_corr_str = ""
     elif scannerParams.get("stir_recons_att_corr") == 1:
         att_corr_str = (
@@ -723,15 +614,32 @@ def OSEM3D_recons(
         att_corr_str = ""
     """
     
+    #THIS IS TO CORRECT FROM IMG DIRECTLY, SEEMS TO NOT BE NECESSARY:
+    """
     if scannerParams.get("stir_norm_from_att_map") == 1:
         att_corr_str = (
-            "Bin Normalisation From Attenuation Image:= \n"
+            #"Bin Normalisation From Attenuation Image:= \n"
+            "Bin Normalisation type := From Attenuation Image \n"
+            + "Bin Normalisation From Attenuation Image:= \n"
             + "attenuation_image_filename := " 
             + join(output_dir, 'mu_map.hv')
             + "\n"
             + "End Bin Normalisation From Attenuation Image := \n"
         )
-    elif scannerParams.get("stir_recons_att_corr") == 1:
+    """
+    #WARNING: UNIFY IF STIR_ATT IS USED.
+    #if scannerParams.get("stir_norm_from_att_map") == 1:
+    if scannerParams.get("attenuation_mode") == 1:
+        att_corr_str = (
+            "Bin Normalisation type := From ProjData \n"
+            + "Bin Normalisation From ProjData := \n"
+            + "normalisation projdata filename:= "
+            + att_stir
+            + "\n"
+            + "End Bin Normalisation From ProjData:= \n"
+        )
+    
+    elif scannerParams.get("attenuation_mode") == 2:
         att_corr_str = (
             "Bin Normalisation type := From ProjData \n"
             + "Bin Normalisation From ProjData := \n"
@@ -802,7 +710,7 @@ def OSEM3D_recons(
         + "Projector Pair Using Matrix Parameters := \n"
         + "Matrix type := Ray Tracing \n"
         + "Ray tracing matrix parameters := \n"
-        + "number of rays in tangential direction to trace for each bin:= 5 \n"
+        + "number of rays in tangential direction to trace for each bin:= 5 \n" #WARNING! ORIGINAL WAS 5
         + "End Ray tracing matrix parameters := \n"
         + "End Projector Pair Using Matrix Parameters := \n\n"
         + att_corr_str
@@ -863,5 +771,6 @@ def OSEM3D_recons(
 
     #output = tools.anything_to_hdr_convert(output, log_file)
     output = tools.convert_hv_to_nii(output, log_file)
+    tools.flip_rec_nifti(recFileName + "_" + str(scannerParams.get("numberOfIterations")) + ".nii", join(output_dir, "reconstruction.nii")) #WARNING! REMOVE IF THIS FAILS. THIS FLIPS THE FINAL REC IN 0 AXIS
 
     return output
